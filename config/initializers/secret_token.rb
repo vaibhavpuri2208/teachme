4efab3fd256236bcc8f325429562f0f3af900967
vaibhav puri <vaibhav@taggito.com>
2013-03-15 11:59:59 +0100

    all files initialised

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teachme::Application.config.secret_token = '63712cde0fa103ba6e12b4d4e59525a4112de510a5864dc0f978fb10966bdacd51f1c262a30e908523a4bed94b7bcd354c0f312af04a426162e67cc39bb9b82c'
